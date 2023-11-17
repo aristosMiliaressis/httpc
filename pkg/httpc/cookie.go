30098442b4d300e755345020429154834c09a113
unknown <aristosmiliaresis@gmail.com>
2023-11-17 14:13:34 +0200

    concurrency fix

--- pkg/httpc/cookie.go
+++ pkg/httpc/cookie.go
@@ -6,15 +6,18 @@ func (c *HttpClient) GetCookieJar() map[string]string {
 	c.cookieJarMutex.RLock()
 	defer c.cookieJarMutex.RUnlock()
 
-	return c.cookieJar
+	jarCopy := c.cookieJar
+
+	return jarCopy
 }
 
 func (c *HttpClient) AddCookie(name string, value string) {
 	c.cookieJarMutex.Lock()
+	defer c.cookieJarMutex.Unlock()
+
 	if c.cookieJar[name] != value {
 		c.cookieJar[name] = value
 	}
-	c.cookieJarMutex.Unlock()
 }
 
 func ContainsCookie(req *http.Request, cookieName string) bool {

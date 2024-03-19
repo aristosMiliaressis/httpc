bb6534bd85341e48f5a2cef10be1cba4078109ea
unknown <aristosmiliaresis@gmail.com>
2024-03-19 13:31:09 +0200

    MessageLog.SelectInt64

--- pkg/httpc/message-log.go
+++ pkg/httpc/message-log.go
@@ -47,3 +47,20 @@ func (log MessageLog) Select(filter func(msg *MessageDuplex) string) []string {
 
 	return selected
 }
+
+func (log MessageLog) SelectInt64(filter func(msg *MessageDuplex) int64) []int64 {
+	selected := []int64{}
+	for _, msg := range log {
+		for {
+			selected = append(selected, filter(msg))
+
+			if msg.Prev == nil {
+				break
+			}
+
+			msg = msg.Prev
+		}
+	}
+
+	return selected
+}
